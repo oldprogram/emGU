e67eeec171834cefbc52fdf3bb4284a12593e28d
litao <953521476@qq.com>
2015-03-12 12:48:32 +0800

    车辆运动检测
    从视频里读图处理

---
+++ base knowledge/运动检测/车辆运动检测/emug1/Form1.Designer.cs
@@ -0,0 +1,141 @@
+﻿namespace emug1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
+            this.motionImageBox = new Emgu.CV.UI.ImageBox();
+            this.forgroundImageBox = new Emgu.CV.UI.ImageBox();
+            this.capturedImageBox = new Emgu.CV.UI.ImageBox();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.播放视频ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.tableLayoutPanel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.motionImageBox)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.forgroundImageBox)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.capturedImageBox)).BeginInit();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // tableLayoutPanel1
+            // 
+            this.tableLayoutPanel1.ColumnCount = 3;
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 325F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel1.Controls.Add(this.motionImageBox, 0, 0);
+            this.tableLayoutPanel1.Controls.Add(this.forgroundImageBox, 0, 0);
+            this.tableLayoutPanel1.Controls.Add(this.capturedImageBox, 0, 0);
+            this.tableLayoutPanel1.Location = new System.Drawing.Point(1, 64);
+            this.tableLayoutPanel1.Name = "tableLayoutPanel1";
+            this.tableLayoutPanel1.RowCount = 1;
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel1.Size = new System.Drawing.Size(986, 379);
+            this.tableLayoutPanel1.TabIndex = 0;
+            // 
+            // motionImageBox
+            // 
+            this.motionImageBox.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.motionImageBox.Location = new System.Drawing.Point(658, 3);
+            this.motionImageBox.Name = "motionImageBox";
+            this.motionImageBox.Size = new System.Drawing.Size(325, 373);
+            this.motionImageBox.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.motionImageBox.TabIndex = 5;
+            this.motionImageBox.TabStop = false;
+            // 
+            // forgroundImageBox
+            // 
+            this.forgroundImageBox.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.forgroundImageBox.Location = new System.Drawing.Point(333, 3);
+            this.forgroundImageBox.Name = "forgroundImageBox";
+            this.forgroundImageBox.Size = new System.Drawing.Size(319, 373);
+            this.forgroundImageBox.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.forgroundImageBox.TabIndex = 4;
+            this.forgroundImageBox.TabStop = false;
+            // 
+            // capturedImageBox
+            // 
+            this.capturedImageBox.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.capturedImageBox.Location = new System.Drawing.Point(3, 3);
+            this.capturedImageBox.Name = "capturedImageBox";
+            this.capturedImageBox.Size = new System.Drawing.Size(324, 373);
+            this.capturedImageBox.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.capturedImageBox.TabIndex = 3;
+            this.capturedImageBox.TabStop = false;
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.播放视频ToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(988, 25);
+            this.menuStrip1.TabIndex = 1;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // 播放视频ToolStripMenuItem
+            // 
+            this.播放视频ToolStripMenuItem.Name = "播放视频ToolStripMenuItem";
+            this.播放视频ToolStripMenuItem.Size = new System.Drawing.Size(68, 21);
+            this.播放视频ToolStripMenuItem.Text = "播放视频";
+            this.播放视频ToolStripMenuItem.Click += new System.EventHandler(this.播放视频ToolStripMenuItem_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(988, 443);
+            this.Controls.Add(this.tableLayoutPanel1);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.tableLayoutPanel1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.motionImageBox)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.forgroundImageBox)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.capturedImageBox)).EndInit();
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
+        private Emgu.CV.UI.ImageBox motionImageBox;
+        private Emgu.CV.UI.ImageBox forgroundImageBox;
+        private Emgu.CV.UI.ImageBox capturedImageBox;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem 播放视频ToolStripMenuItem;
+
+
+    }
+}
+
---
+++ base knowledge/运动检测/车辆运动检测/emug1/Form1.cs
@@ -0,0 +1,133 @@
+﻿using System;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.Windows.Forms;
+using Emgu.CV;//PS:调用的Emgu dll  
+using Emgu.CV.Structure;
+using Emgu.CV.VideoSurveillance;
+
+namespace emug1
+{
+    public partial class Form1 : Form
+    {
+        private Capture _capture2 = null;
+        private MotionHistory _motionHistory;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        Image<Bgr, Byte> image1 = null;
+        Image<Gray, Byte> image2 = null;
+        int count = 0;
+        int rate = 10;
+        private IBGFGDetector<Bgr> _forgroundDetector;
+
+        private void ProcessFrame(object sender, EventArgs e)
+        {
+            count++;
+            
+            using (image1 = _capture2.QueryFrame())
+            using (image2 = _capture2.QueryGrayFrame())
+            using (MemStorage storage = new MemStorage()) //create storage for motion components
+            {
+                image1 = image1.Resize(0.5, 0);
+                image2 = image2.Resize(0.5, 0);
+                capturedImageBox.Image = image1;
+                //update the motion history
+                _motionHistory.Update(image2); 
+                forgroundImageBox.Image = image2;
+                
+                //if (count > int.MaxValue - 2) count = 0;
+                //if (count % rate != 0) return;
+                #region get a copy of the motion mask and enhance its color
+                double[] minValues, maxValues;
+                Point[] minLoc, maxLoc;
+                _motionHistory.Mask.MinMax(out minValues, out maxValues, out minLoc, out maxLoc);
+                Image<Gray, Byte> motionMask = _motionHistory.Mask.Mul(255.0 / maxValues[0]);
+                #endregion
+
+                //create the motion image 
+                Image<Bgr, Byte> motionImage = new Image<Bgr, byte>(motionMask.Size);
+                //display the motion pixels in blue (first channel)
+                motionImage[0] = motionMask;
+
+                //Threshold to define a motion area, reduce the value to detect smaller motion
+                double minArea = 1;
+
+                storage.Clear(); //clear the storage
+                Seq<MCvConnectedComp> motionComponents = _motionHistory.GetMotionComponents(storage);
+
+                //iterate through each of the motion component
+                foreach (MCvConnectedComp comp in motionComponents)
+                {
+                    //reject the components that have small area;
+                    if (comp.area < minArea) continue;
+
+                    // find the angle and motion pixel count of the specific area
+                    double angle, motionPixelCount;
+                    _motionHistory.MotionInfo(comp.rect, out angle, out motionPixelCount);
+
+                    //reject the area that contains too few motion
+                    if (motionPixelCount < comp.area * 0.05) continue;
+
+                    //Draw each individual motion in red
+                    DrawMotion(motionImage, comp.rect, angle, new Bgr(Color.Red));
+                }
+
+                // find and draw the overall motion angle
+                double overallAngle, overallMotionPixelCount;
+                _motionHistory.MotionInfo(motionMask.ROI, out overallAngle, out overallMotionPixelCount);
+                DrawMotion(motionImage, motionMask.ROI, overallAngle, new Bgr(Color.Green));
+
+                //Display the amount of motions found on the current image
+                //UpdateText(String.Format("Total Motions found: {0}; Motion Pixel count: {1}", motionComponents.Total, overallMotionPixelCount));
+
+                //Display the image of the motion
+                motionImageBox.Image = motionImage;
+            }
+        }
+
+        private static void DrawMotion(Image<Bgr, Byte> image, Rectangle motionRegion, double angle, Bgr color)
+        {
+            float circleRadius = (motionRegion.Width + motionRegion.Height) >> 2;
+            Point center = new Point(motionRegion.X + motionRegion.Width >> 1, motionRegion.Y + motionRegion.Height >> 1);
+
+            CircleF circle = new CircleF(
+               center,
+               circleRadius);
+
+            int xDirection = (int)(Math.Cos(angle * (Math.PI / 180.0)) * circleRadius);
+            int yDirection = (int)(Math.Sin(angle * (Math.PI / 180.0)) * circleRadius);
+            Point pointOnCircle = new Point(
+                center.X + xDirection,
+                center.Y - yDirection);
+            LineSegment2D line = new LineSegment2D(center, pointOnCircle);
+
+            image.Draw(circle, color, 1);
+            image.Draw(line, color, 2);
+        }
+
+        private void Form1_FormClosed(object sender, FormClosedEventArgs e)
+        {
+            Application.Idle -= new EventHandler(ProcessFrame);
+        }
+
+        private void 播放视频ToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog openFileDialog = new OpenFileDialog();
+            openFileDialog.FilterIndex = 5;
+            openFileDialog.Filter = "AVI文件|*.avi|RMVB文件|*.rmvb|WMV文件|*.wmv|MKV文件|*.mkv|所有文件|*.*";
+            if (openFileDialog.ShowDialog() == DialogResult.OK)
+            {
+                Application.Idle += new EventHandler(ProcessFrame);  //播放的函数句柄
+                _capture2 = new Capture(openFileDialog.FileName);
+                _motionHistory = new MotionHistory(
+                    1.0, //in second, the duration of motion history you wants to keep
+                    0.05, //in second, maxDelta for cvCalcMotionGradient
+                    0.5); //in second, minDelta for cvCalcMotionGradient
+            }
+        }
+    }
+}
